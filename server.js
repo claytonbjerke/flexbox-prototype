f99c2324802f775e5305aa7731b1e3c2fa78b2cf
claytonbjerke <cbjerkeuco@gmail.com>
2016-04-22 22:35:48 -0500

    minor

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.set('port', 8080);
+app.set('port', (process.env.PORT || 8080));
 app.use(express.static(path.join(__dirname, 'src')));
 app.listen(app.get('port'), function () {
     console.log('port: ', app.get('port'));
